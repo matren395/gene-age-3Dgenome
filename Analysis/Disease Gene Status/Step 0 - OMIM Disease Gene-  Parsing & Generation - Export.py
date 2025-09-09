44bd671b5885d52bf6900547619438ec186c7801
Daniel Marten <marten@broadinstitute.org>
2025-09-09 10:57:25 -0400

    Add disease & knockout
    Restructure the Analysis folder, and add files for Disease and Knockout

---
+++ Analysis/Disease Gene Status/Step 0 - OMIM Disease Gene-  Parsing & Generation - Export.py
@@ -0,0 +1,305 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+## Daniel Marten
+## Read in OMIM Disease Data, then annotate to existing tables
+
+import math
+import os
+import re
+import statistics
+import sys
+import warnings
+from collections import Counter
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import qtl.io as io
+import qtl.norm as norm
+import scipy
+import scipy.stats as scistats
+import seaborn as sns
+import sklearn
+import statsmodels.api as sm
+import statsmodels.stats.weightstats as sm_stats
+from cmapPy.pandasGEXpress.parse_gct import parse as tpm_parser
+from matplotlib.pyplot import figure
+from statsmodels.formula.api import ols as formula_OLS
+
+pd.set_option("display.max_columns", None)
+
+
+# Import list of OMIM Disease Genes, as provided by Sarah Stenton
+disease_list = pd.read_csv(
+    "/Users/marten/Downloads/omim_disease_genes.txt",
+    sep="\t",
+)
+
+
+# We do not include Phenotypes denoted with ?, [, or {
+# Analysis does not include digenic modes of inheritance
+# And classifies X-Linked as a type of X-Linked Dominant (for comparisons to Autosomal)
+disease_list["Phenotype_Start"] = [xi[0] for xi in disease_list.Phenotypes]
+disease_list = disease_list[~disease_list["Phenotype_Start"].isin(["?", "[", "{"])]
+
+disease_list = disease_list[disease_list.moi != "digenic"]
+
+disease_list["moi"] = ["XLD" if xi == "XL" else xi for xi in disease_list["moi"]]
+
+
+# Group genes by all Modes Of Inheritance, confirm that some genes have multiple
+grouped_list = disease_list.groupby("Ensembl Gene ID")["moi"].unique().to_frame()
+
+
+# Counts for length of unique MOIs
+grouped_list["moi_counts"] = [len(xi) for xi in grouped_list["moi"]]
+# Filter to only genes with plural for later filtering
+grouped_list = grouped_list[grouped_list.moi_counts > 1]
+
+
+# Sorted set, for consistency and to see what counts we have
+grouped_list["moi_sorted_set"] = [list(set(sorted(xi))) for xi in grouped_list.moi]
+grouped_list["moi_sorted_set"].value_counts()
+
+
+# Defined Mixed MOI for later classifications as "mixed sex-linked" or so on
+grouped_list["mixed_moi"] = None
+
+for xi, yi in grouped_list.iterrows():
+    if "Digenic" in yi.moi_sorted_set:
+        raise Exception("Previously filtered to no digenic...")
+    elif "XLR" in yi.moi_sorted_set:
+        grouped_list.loc[xi, "mixed_moi"] = "Mixed Sex-Linked"
+    else:
+        grouped_list.loc[xi, "mixed_moi"] = "Mixed Autosomal"
+
+
+# Annotated this Mixed Phenotypes/MOIs onto the Disease Table
+disease_list["mixed_phenotypes"] = [
+    xi in list(grouped_list.index) for xi in disease_list["Ensembl Gene ID"]
+]
+
+disease_list["moi"] = disease_list["moi"].replace("AD/AR", "AD/AR MOI")
+disease_list.value_counts(["moi", "mixed_phenotypes"])
+
+
+# Annotate properly mixed MOIs
+for xi, yi in disease_list.iterrows():
+    if yi["Ensembl Gene ID"] in list(grouped_list.index):
+        disease_list.loc[xi, "moi"] = grouped_list.loc[
+            yi["Ensembl Gene ID"], "mixed_moi"
+        ]
+    else:
+        pass
+
+
+# Only interested in those with Ensembl Gene ID
+disease_list = disease_list[~disease_list["Ensembl Gene ID"].isna()]
+disease_list = disease_list.set_index("Ensembl Gene ID")
+# Binary to be used later - for if a gene is associated with any disease, regardless of moi
+disease_list["disease_gene_binary"] = True
+display(disease_list)
+
+
+## Set up Translation Dictionaries
+
+translationDict = pd.read_csv(
+    r"/Users/marten/Downloads/biomart_export_grch38_ens93_jul2018_20231121.txt",
+    sep="\t",
+)  # Using GRCh38 mapping
+# ENSGs remain stable between versions (e.g., ENSG000567 does not become ENSG000771).
+# What changes is the mapping to transcripts and proteins.
+
+translationDict["Ensembl_gene_length"] = (
+    translationDict["Gene end (bp)"] - translationDict["Gene start (bp)"]
+)
+
+translationDict.rename(
+    columns={
+        "Gene stable ID": "ENSG",
+        "Transcript stable ID": "ENST",
+        "Protein stable ID": "ENSP",
+    },
+    inplace=True,
+)
+
+translationDict = translationDict[["ENSG", "ENST", "ENSP", "Ensembl_gene_length"]]
+
+# Make different dictionary versions indexed on ENSG, ENST, and ENSP.
+transEnsg = translationDict.copy()
+transEnsg.set_index("ENSG", inplace=True)
+
+transEnst = translationDict.copy()
+transEnst.set_index("ENST", inplace=True)
+
+transEnsp = translationDict.copy()
+transEnsp.set_index("ENSP", inplace=True)
+
+
+# Table S1A from the 3DG publication, as a source to annotate data onto
+s1a = pd.read_excel("/Users/marten/Downloads/Table_S1.xlsm", skiprows=4)
+s1a_origin = s1a.copy()
+s1a = s1a.set_index("Protein_ID").join(transEnsp)
+display(s1a)
+
+
+# Filter to only relevant columns for later joining
+omim_join = disease_list[
+    [
+        "Gene/Locus And Other Related Symbols",
+        "Gene Name",
+        "Approved Gene Symbol",
+        "Phenotypes",
+        "moi",
+    ]
+]
+
+# Our analysis is not phenotype-specific, only MOI
+omim_join = (
+    omim_join.groupby(omim_join.index)[
+        [
+            "Gene/Locus And Other Related Symbols",
+            "Gene Name",
+            "Approved Gene Symbol",
+            "Phenotypes",
+            "moi",
+        ]
+    ]
+    .agg(["unique"])
+    .droplevel(1, axis=1)
+)
+
+omim_join["disease_gene_inheritance"] = [str(xi[0]) for xi in omim_join["moi"]]
+omim_join["disease_gene_binary"] = True
+
+
+# Only join by ENSG
+s1a_ENSG = s1a.reset_index(drop=False).set_index("ENSG").join(omim_join)
+display(s1a_ENSG)
+
+
+# Binary is True if a gene is any sort of disease gene
+s1a_ENSG["disease_gene_binary"] = [xi == True for xi in s1a_ENSG["disease_gene_binary"]]
+display(s1a_ENSG)
+
+
+# Further annotations - descriptive notes for Non-Disease Genes
+
+s1a_ENSG = s1a_ENSG.reset_index(drop=False)
+
+for xi, yi in s1a_ENSG.iterrows():
+
+    # Set default to Non-Disease, for those not joined to
+    if str(yi.moi) in ["None", "NaN", "nan"]:
+        s1a_ENSG.loc[xi, "disease_gene_inheritance"] = "Non-Disease"
+
+    # If default and on Sex Chromosome, note as such!
+    if (str(yi["Chr"]) in ["chrX", "chrY"]) & (
+        s1a_ENSG.loc[xi, "disease_gene_inheritance"] == "Non-Disease"
+    ):
+        s1a_ENSG.loc[xi, "disease_gene_inheritance"] = "Non-Disease Sex-Linked"
+
+    # For ALL Unannotated Genes, note as Excluded
+    if str(yi["Protein_ID"])[:4] != "ENSP":
+        s1a_ENSG.loc[xi, "disease_gene_inheritance"] = "EXCLUDED-Unannotated"
+
+s1a_ENSG = s1a_ENSG.set_index("Protein_ID")
+
+
+s1a_ENSG = s1a_ENSG.drop("moi", axis=1)
+
+
+## Now for S3A Deliverables
+# Essentially same work as above
+
+s3a = pd.read_csv(
+    "~/Serious_Work/Batch_Scripts/unannotated-genes-with-all/Tables S3/tables_3a_marten_synteny_20241007.tsv",
+    sep="\t",
+)
+s3a = s3a.set_index("Identifier").join(transEnsp)
+
+
+# Index by ENSG, join to OMIM table
+s3a_ENSG = (
+    s3a.reset_index(drop=False)
+    .set_index("ENSG")
+    .join(omim_join)
+    .reset_index(drop=False)
+    .set_index("Identifier")
+)
+
+s3a_ENSG["disease_gene_binary"] = [xi == True for xi in s3a_ENSG["disease_gene_binary"]]
+s3a_ENSG
+
+s3a_ENSG
+
+
+# Annotater details about non-disease on
+
+s3a_ENSG_q = s3a_ENSG.reset_index(drop=False)
+
+for xi, yi in s3a_ENSG_q.iterrows():
+
+    #     s3a_ENSG_q[aguwrin]
+
+    # Set default to Non-Disease
+    if str(yi.moi) in ["None", "NaN", "nan"]:
+        s3a_ENSG_q.loc[xi, "disease_gene_inheritance"] = "Non-Disease"
+
+    # If default and on Sex Chromosome, note as such!
+    if (str(yi["chr"]) in ["chrX", "chrY"]) & (
+        s3a_ENSG_q.loc[xi, "disease_gene_inheritance"] == "Non-Disease"
+    ):
+        s3a_ENSG_q.loc[xi, "disease_gene_inheritance"] = "Non-Disease Sex-Linked"
+
+    # For ALL Unannotated Genes, note as Excluded
+    if "Unannotated" in str(yi["annotation"]):
+        s3a_ENSG_q.loc[xi, "disease_gene_inheritance"] = "EXCLUDED-Unannotated"
+
+    # For ALL Intergenic Controls, note as Excluded
+    if str(yi["annotation"]) in ["orf", "norf"]:
+        s3a_ENSG_q.loc[xi, "disease_gene_inheritance"] = "EXCLUDED-Intergenic Control"
+
+
+s3a_ENSG_q = s3a_ENSG_q.drop("moi", axis=1).set_index("Identifier")
+
+s3a_ENSG_q
+
+
+# Excluded & Plotted Logic
+# For future decisions to not plot Mixed MOIs
+
+
+s3a_ENSG_q["Excluded"] = [
+    "EXCLUDED" in xi for xi in s3a_ENSG_q.disease_gene_inheritance
+]
+
+s3a_ENSG_q["GRCh38_Expression_Plotting"] = [
+    not any(["AD/AR" in xi, "EXCLUDED" in xi, "Mixed" in xi])
+    for xi in s3a_ENSG_q.disease_gene_inheritance
+]
+
+s3a_ENSG_q["Excluded"].value_counts()
+
+s3a_ENSG_q["GRCh38_Expression_Plotting"].value_counts()
+
+s3a_ENSG_q.value_counts(["Excluded", "GRCh38_Expression_Plotting"]).sort_index()
+
+
+s1a_ENSG["Excluded"] = ["EXCLUDED" in xi for xi in s1a_ENSG.disease_gene_inheritance]
+
+s1a_ENSG["GRCh38_Expression_Plotting"] = [
+    not any(["AD/AR" in xi, "EXCLUDED" in xi, "Mixed" in xi])
+    for xi in s1a_ENSG.disease_gene_inheritance
+]
+
+s1a_ENSG["Excluded"].value_counts()
+
+s1a_ENSG["GRCh38_Expression_Plotting"].value_counts()
+
+s1a_ENSG.value_counts(["Excluded", "GRCh38_Expression_Plotting"]).sort_index()
+
+
+s3a_ENSG_q.to_csv("marten_s3a_omimdisease_20250314.tsv", sep="\t")
+s1a_ENSG.to_csv("marten_s1a_omimdisease_20250314.tsv", sep="\t")
---
+++ Analysis/Knockout/Step 1 - RNASeq Knockout  & Disease Statistics Calculator 3 and 5 Era Modification - 20250613.py
@@ -0,0 +1,447 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+# Significance Stats for Knockout Analysis
+
+import pandas as pd
+import scipy
+import statsmodels
+from IPython.display import display  # type: ignore
+
+
+# Display options
+pd.set_option("display.max_columns", None)
+
+
+# Simplified: optionally compute group means.
+
+
+def sig_table(
+    eras_input,
+    tissue_input,
+    incoming_df,
+    era_col,
+    verbose=False,
+    do_mean=False,
+):
+
+    # Create containers that will become the DataFrame.
+
+    dfResults = pd.DataFrame()
+
+    left = []
+    right = []
+    floatvals = []
+    pvals = []
+    # removed unused accumulators
+
+    for x1_idx in range(len(eras_input)):
+        for x2 in eras_input[x1_idx:]:
+            x1 = eras_input[x1_idx]
+            # x1 and x2 iterate over all combinations of era labels.
+            if verbose:
+                print(f"Comparing from: {x1} & {x2}")
+
+            for y1_idx in range(len(tissue_input)):
+                for y2 in tissue_input[y1_idx:]:
+                    y1 = tissue_input[y1_idx]
+                    # print(x1,x2)
+                    # print(y1,y2)
+                    # y1 and y2 iterate over all combinations of tissue-group labels.
+                    if verbose:
+                        print(f"comparing ({x1},{y1}) to ({x2},{y2})")
+                    if (x1 == x2) and (y1 == y2):
+                        if verbose:
+                            print("Duplicate - pass")
+                    else:
+
+                        xinput = incoming_df[
+                            (incoming_df[era_col] == x1) & (incoming_df.Knockout == y1)
+                        ]
+                        yinput = incoming_df[
+                            (incoming_df[era_col] == x2) & (incoming_df.Knockout == y2)
+                        ]
+
+                        if do_mean == True:
+                            if "Name" in xinput.columns:
+                                xinput = xinput.groupby("Name")["Mean(Count)"].mean()
+                                yinput = yinput.groupby("Name")["Mean(Count)"].mean()
+                            else:
+                                xinput = xinput.groupby(xinput.index)[
+                                    "Mean(Count)"
+                                ].mean()
+                                yinput = yinput.groupby(yinput.index)[
+                                    "Mean(Count)"
+                                ].mean()
+                        else:
+                            xinput = xinput["Mean(Count)"]
+                            yinput = yinput["Mean(Count)"]
+
+                        x_mean, x_median, x_std, x_stderr, x_count = (
+                            xinput.mean(),
+                            xinput.median(),
+                            xinput.std(),
+                            xinput.sem(),
+                            len(xinput),
+                        )
+
+                        y_mean, y_median, y_std, y_stderr, y_count = (
+                            yinput.mean(),
+                            yinput.median(),
+                            yinput.std(),
+                            yinput.sem(),
+                            len(yinput),
+                        )
+
+                        statistics_object_list = scipy.stats.mannwhitneyu(
+                            x=xinput, y=yinput, nan_policy="omit"
+                        )
+
+                        left.append(
+                            (x1, y1, x_mean, x_median, x_std, x_stderr, x_count)
+                        )
+                        right.append(
+                            (x2, y2, y_mean, y_median, y_std, y_stderr, y_count)
+                        )
+                        floatvals.append(statistics_object_list[0])
+                        pvals.append(statistics_object_list[1])
+
+    dfResults["Left Status"] = [tup[0] for tup in left]
+    dfResults["Left Knockout_Group"] = [tup[1] for tup in left]
+    dfResults["Left Mean"] = [tup[2] for tup in left]
+    dfResults["Left Median"] = [tup[3] for tup in left]
+    dfResults["Left STDev"] = [tup[4] for tup in left]
+    dfResults["Left STDerr"] = [tup[5] for tup in left]
+    dfResults["Left Count"] = [tup[-1] for tup in left]
+
+    dfResults["Right Status"] = [tup[0] for tup in right]
+    dfResults["Right Knockout_Group"] = [tup[1] for tup in right]
+    dfResults["Right Mean"] = [tup[2] for tup in right]
+    dfResults["Right Median"] = [tup[3] for tup in right]
+    dfResults["Right STDev"] = [tup[4] for tup in right]
+    dfResults["Right STDerr"] = [tup[5] for tup in right]
+    dfResults["Right Count"] = [tup[-1] for tup in right]
+
+    dfResults["Floats"] = floatvals
+    dfResults["pvals"] = pvals
+    dfResults.sort_values(by="pvals", inplace=True)
+    dfResults.reset_index(drop=True, inplace=True)
+
+    display(dfResults)
+
+    return dfResults
+
+
+# Table containing mean read count data (with RNASeq Knockout annotation)
+df5 = pd.read_csv(
+    "Marten Knockout RNASeq 20250529/tables/full_source_genes_with_KOexpression.tsv",
+    sep="\t",
+    index_col="Identifier",
+)
+
+
+# Filter to genes flagged for GRCh38 expression plotting (relevant subset).
+df5_nonexcl = df5[df5.GRCh38_Expression_Plotting == True]
+
+
+# These serve as the tissue categories (functionally equivalent).
+# Still iterate over disease status and evolutionary category.
+mean_stats = ["DKO_KO_mean", "Hap1_WT_mean", "Wapl_KO_mean", "SCC4_KO_mean"]
+
+
+df_meancount = df5_nonexcl[
+    [
+        "evolutionary_category",
+        "evolutionary_category_3era",
+        "disease_gene_inheritance_merged",
+    ]
+    + mean_stats
+]
+
+
+unique_eras = {
+    "1-Ancient": "1-Ancient",
+    "2-Metazoan": "2-Metazoan",
+    "3-Chordate": "3-Chordate_5era",
+    "4-Mammal": "4-Mammal_5era",
+    "5-Primate": "5-Primate_5era",
+}
+
+
+df_meancount = df_meancount.rename(
+    {"evolutionary_category": "evolutionary_category_5era"}, axis=1
+)
+df_meancount["evolutionary_category_5era"] = [
+    unique_eras[xi] for xi in df_meancount.evolutionary_category_5era
+]
+
+
+melt_df = pd.melt(
+    frame=df_meancount,
+    id_vars=[
+        "evolutionary_category_3era",
+        "evolutionary_category_5era",
+        "disease_gene_inheritance_merged",
+    ],
+    var_name="Knockout",
+    value_name="Mean(Count)",
+    ignore_index=False,
+)
+melt_df["joint_disease_era_3era"] = [
+    f"{xi[0]}_{xi[1]}"
+    for xi in zip(
+        melt_df.evolutionary_category_3era, melt_df.disease_gene_inheritance_merged
+    )
+]
+melt_df["joint_disease_era_5era"] = [
+    f"{xi[0]}_{xi[1]}"
+    for xi in zip(
+        melt_df.evolutionary_category_5era, melt_df.disease_gene_inheritance_merged
+    )
+]
+
+
+# Groups and arguments for the statistics method.
+eragroups_3era = sorted(melt_df.evolutionary_category_3era.unique())
+eragroups_5era = sorted(melt_df.evolutionary_category_5era.unique())
+
+tissueCatKeys = sorted(melt_df.Knockout.unique())
+
+joint_groups_3era = sorted(melt_df.joint_disease_era_3era.unique())
+joint_groups_5era = sorted(melt_df.joint_disease_era_5era.unique())
+
+disease_groups = sorted(melt_df.disease_gene_inheritance_merged.unique())
+
+
+tissue_dict = {}
+
+for xi in tissueCatKeys:
+    tissue_dict[xi] = [xi]
+
+
+# Calculations by knockout group and Evolutionary Category
+
+df_era_3era = sig_table(
+    eras_input=eragroups_3era,
+    tissue_input=tissueCatKeys,
+    incoming_df=melt_df,
+    era_col="evolutionary_category_3era",
+    do_mean=False,
+    verbose=False,
+)
+
+# Already mean-aggregated; no need to recompute.
+df_era_5era = sig_table(
+    eras_input=eragroups_5era,
+    tissue_input=tissueCatKeys,
+    incoming_df=melt_df,
+    era_col="evolutionary_category_5era",
+    do_mean=False,
+    verbose=False,
+)
+
+
+# Calculations by knockout group and disease status
+
+df_disease = sig_table(
+    eras_input=disease_groups,
+    tissue_input=tissueCatKeys,
+    incoming_df=melt_df,
+    era_col="disease_gene_inheritance_merged",
+    do_mean=False,
+    verbose=True,
+)
+
+
+# Ultra-simplified significance stats — only between treatment groups.
+# Fewer iterations: compare only within a single column (treatment groups).
+
+
+def sig_table_simplified(tissue_input, incoming_df, verbose=False, do_mean=False):
+
+    # Create empty lists that will become the dataframe
+
+    dfResults = pd.DataFrame()
+
+    left = []
+    right = []
+    floatvals = []
+    pvals = []
+
+    for y1_idx in range(len(tissue_input)):
+        for y2 in tissue_input[y1_idx:]:
+            y1 = tissue_input[y1_idx]
+            if verbose:
+                print(f"comparing ({y1}) to ({y2})")
+            if y1 == y2:
+                if verbose:
+                    print("Duplicate - pass")
+            else:
+
+                xinput = incoming_df[(incoming_df.Knockout == y1)]
+                yinput = incoming_df[(incoming_df.Knockout == y2)]
+
+                if do_mean == True:
+                    if "Name" in xinput.columns:
+                        xinput = xinput.groupby("Name")["Mean(Count)"].mean()
+                        yinput = yinput.groupby("Name")["Mean(Count)"].mean()
+                    else:
+                        xinput = xinput.groupby(xinput.index)["Mean(Count)"].mean()
+                        yinput = yinput.groupby(yinput.index)["Mean(Count)"].mean()
+                else:
+                    xinput = xinput["Mean(Count)"]
+                    yinput = yinput["Mean(Count)"]
+
+                x_mean, x_median, x_std, x_stderr, x_count = (
+                    xinput.mean(),
+                    xinput.median(),
+                    xinput.std(),
+                    xinput.sem(),
+                    len(xinput),
+                )
+
+                y_mean, y_median, y_std, y_stderr, y_count = (
+                    yinput.mean(),
+                    yinput.median(),
+                    yinput.std(),
+                    yinput.sem(),
+                    len(yinput),
+                )
+
+                statistics_object_list = scipy.stats.mannwhitneyu(
+                    x=xinput, y=yinput, nan_policy="omit"
+                )
+
+                left.append(
+                    ("All_Genes", y1, x_mean, x_median, x_std, x_stderr, x_count)
+                )
+                right.append(
+                    ("All_Genes", y2, y_mean, y_median, y_std, y_stderr, y_count)
+                )
+                floatvals.append(statistics_object_list[0])
+                pvals.append(statistics_object_list[1])
+
+    dfResults["Left Status"] = [tup[0] for tup in left]
+    dfResults["Left Knockout_Group"] = [tup[1] for tup in left]
+    dfResults["Left Mean"] = [tup[2] for tup in left]
+    dfResults["Left Median"] = [tup[3] for tup in left]
+    dfResults["Left STDev"] = [tup[4] for tup in left]
+    dfResults["Left STDerr"] = [tup[5] for tup in left]
+    dfResults["Left Count"] = [tup[-1] for tup in left]
+
+    dfResults["Right Status"] = [tup[0] for tup in right]
+    dfResults["Right Knockout_Group"] = [tup[1] for tup in right]
+    dfResults["Right Mean"] = [tup[2] for tup in right]
+    dfResults["Right Median"] = [tup[3] for tup in right]
+    dfResults["Right STDev"] = [tup[4] for tup in right]
+    dfResults["Right STDerr"] = [tup[5] for tup in right]
+    dfResults["Right Count"] = [tup[-1] for tup in right]
+
+    dfResults["Floats"] = floatvals
+    dfResults["pvals"] = pvals
+    dfResults.sort_values(by="pvals", inplace=True)
+    dfResults.reset_index(drop=True, inplace=True)
+
+    display(dfResults)
+
+    return dfResults
+
+
+# Evaluate, only comparing among knockout groups
+df_knockout = sig_table_simplified(
+    tissue_input=tissueCatKeys,
+    incoming_df=melt_df,
+    do_mean=False,
+    verbose=True,
+)
+
+
+with pd.option_context(
+    "display.max_rows",
+    None,
+):
+    display(df_disease)
+
+
+df_disease.value_counts(["Left Status", "Left Count"])
+
+
+df_disease.value_counts(["Right Status", "Right Count"])
+
+
+df_disease.value_counts(["Left Status", "Left Mean"])
+
+
+df_era_3era.value_counts(["Left Status", "Left Count"])
+
+
+# Let's make one merged, de-duplicated `df_era`.
+# From `df_era_5era`, we can exclude those between 1-Ancient and 2-Metazoan.
+# Original count is 130 comparisons for 5-era + knockout status.
+df_era_5era_slim = df_era_5era[
+    (df_era_5era["Left Status"].str.contains("_5era"))
+    | (df_era_5era["Right Status"].str.contains("_5era"))
+]
+display(df_era_5era_slim)  # 108 unique analyses among 5era sets
+
+
+df_era_3era["Analysis_Group"] = "3_Era"
+df_era_5era["Analysis_Group"] = "5_Era_Unique"
+
+df_era = pd.concat([df_era_3era, df_era_5era_slim])
+
+# Combine all results to apply multiple-testing corrections together.
+
+df_disease["Analysis_Group"] = "Disease"
+# df_era['Analysis_Group'] = 'Era' # Already added
+df_knockout["Analysis_Group"] = "Knockout_Only"
+
+df_disease_joint = pd.concat([df_disease, df_era, df_knockout])
+
+
+# Corrected p-values from statsmodels.
+# fixedpvals_disease = statsmodels.stats.multitest.multipletests(df_disease['pvals'],method='fdr_bh')[1]
+# fixedpvals_disease_era = statsmodels.stats.multitest.multipletests(df_disease_era['pvals'],method='fdr_bh')[1]
+fixedpvals_disease_joint = statsmodels.stats.multitest.multipletests(
+    df_disease_joint["pvals"], method="fdr_bh"
+)[1]
+
+df_disease_joint["Adjusted_Pvals_BH"] = fixedpvals_disease_joint
+df_disease_joint["Adjusted_Pvals < 0.05"] = [
+    fp < 0.05 for fp in fixedpvals_disease_joint
+]
+
+
+def comparison_key(df):
+    return (
+        df["Left Status"]
+        + "_"
+        + df["Left Knockout_Group"]
+        + "_"
+        + df["Right Status"]
+        + "_"
+        + df["Right Knockout_Group"]
+    )
+
+
+df_disease_joint["comparison_key"] = comparison_key(df_disease_joint)
+df_disease_joint.set_index("comparison_key", inplace=True)
+
+
+df_disease_joint.to_csv("marten_joint_knockout_significance_calculations.tsv", sep="\t")
+
+
+era_terms = ["1-Ancient", "2-Metazoan", "3-Chordate"]
+excluded_terms = [
+    "Non-Disease",
+    "Dominant",
+    "Recessive",
+    "4-Mammal_5era",
+    "5-Primate_5era",
+    "All_Genes",
+]
+
+df_disease_paper = df_disease_joint[df_disease_joint.Analysis_Group == "3_Era"]
+
+df_disease_paper.to_csv("marten_knockout_era_significance.tsv", sep="\t")
